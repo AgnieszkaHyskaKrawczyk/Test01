61a92b1ad5816b45022cb6d8d747d0afc5f36f98
Agnieszka H.-K <agnieszka.krawczyk@seznam.cz>
2019-04-29 18:30:20 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static int Sum(int a)
+        {
+            if (a == 1)
+            {
+                return 1;
+            }
+            return a * Sum(a - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(6));
+            Console.ReadKey();
+        }
+    }
+}
