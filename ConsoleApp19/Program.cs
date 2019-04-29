44f070dc7c689aed197e3910395478e33ca48560
Agnieszka H.-K <agnieszka.krawczyk@seznam.cz>
2019-04-29 18:21:28 +0200

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clovek pepa = new Clovek();
+            pepa.Vek = 20;
+            Console.WriteLine(pepa.JePlnolety);
+
+            Console.ReadKey();
+        }
+    }
+    class Clovek
+    {
+
+        public bool JePlnolety { get; private set; }
+        private int vek;
+        public int Vek
+        {
+            get { return vek; }
+            set
+            {
+                vek = value;
+                if (vek >= 18)
+                {
+                    JePlnolety = true;
+                }
+                else
+                {
+                    JePlnolety = false;
+                }
+            }
+        }
+    }
+}
